76181c127cb7977f24dcc228891d18ff264a245d
aldosch <git@aldo.io>
2024-07-18 16:55:02 +1000

    read cookies from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { cookies } from "next/headers";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -11,10 +12,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const allCookies = cookies().getAll();
   return (
     <html lang="en">
-      <body className="dark:bg-zinc-900 dark:text-white dark:selection:bg-white/30 dark:selection:text-white">
+      <body className="p-4 dark:bg-zinc-900 dark:text-white dark:selection:bg-white/30 dark:selection:text-white">
         {children}
+        {allCookies.map((cookie) => (
+          <div key={cookie.name}>
+            <p>Cookie name: {cookie.name}</p>
+            <p>Cookie value: {cookie.value}</p>
+          </div>
+        ))}
       </body>
     </html>
   );
